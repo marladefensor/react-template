93f8b05f1239a50bbc3421876e732134ad9dc8b3
Marla Defensor <marladefensor@ibm.com>
2021-12-26 18:52:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
+    </ThemeProvider>
   );
 }
 
